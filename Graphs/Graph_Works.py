8ac3ac4427b4cf2c92ff30ba75eef7d5617b36dc
shenoyrahul444 <shenoyrahul444@gmail.com>
2018-03-30 01:11:49 -0500

    Exlporing Adjecency List

---
+++ Graphs/Graph_Works.py
@@ -0,0 +1,6 @@
+# **************************** Adjecency Matrix *******************
+#  Used to represent a Graph. Good for small dense graphs with lots of edges. Sparsity results in empty cells, so not efficient for sparse.
+
+# ****************** ADJECENCY LIST ***********
+# Adjecency list allows to compactly represent a sparse graph.
+
