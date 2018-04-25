df3ae57ad40d583382c562181e4efbf193dac32d
shenoyrahul444 <shenoyrahul444@gmail.com>
2018-04-24 23:53:06 -0500

    Adding nodes to BST and printing them using traversals

---
+++ BST_Exploration.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, val):
+        self.value = val
+        self.rightChild = None
+        self.leftChild = None
+
+    def __str__(self):
+        node_str = "Node has the value : " + str(self.value)
+
+        if self.rightChild:
+            node_str += "\nRight Child: " + str(self.rightChild.value)
+
+        if self.leftChild:
+            node_str += "\nLeft Child: " + str(self.leftChild.value)
+        return node_str
+
+
+class BST:
+
+    def __init__(self):
+        self.root = None
+
+    def addNode(self,val):
+
+        if self.root == None:
+            self.root = Node(val)
+            return
+
+        self._addNode(self.root,val)
+
+    def _addNode(self,root,val):                 # <-------------- Recursive traversal
+        if root == None:
+            return Node(val)
+        if val > root.value:
+            root.rightChild = self._addNode(root.rightChild,val)
+            return root
+        elif val < root.value:
+            root.rightChild = self._addNode(root.leftChild,val)
+            return root
+
+    def printBST(self):
+        if not self.root:
+            return
+        self._printBST(self.root)
+
+    def _printBST(self,root):                   # <-------------- Recursive traversal
+        if root:
+            self._printBST(root.leftChild)
+            self._printBST(root.rightChild)
+            print(str(root.value))
+
+tree = BST()
+tree.addNode(10)
+tree.addNode(20)
+tree.addNode(30)
+tree.addNode(40)
+
+# curr = tree.root
+# while curr:
+#     print(str(curr.value))
+#     curr = curr.rightChild
+tree.printBST()
